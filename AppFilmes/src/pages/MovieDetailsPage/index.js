1308ca3875bfb7dc0ef782fd69ce62f989185b78
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2023-06-15 19:56:18 -0300

    Add files via upload

---
+++ AppFilmes/src/pages/MovieDetailsPage/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+export default function MovieDetailsPage({ route }){
+    const { filme } = route.params;
+    return (
+        <View>
+            <Text>Detalhes do filme</Text>
+            <Text>{filme.titulo}</Text>
+            <Text>{filme.sinopse}</Text>
+        </View>
+    )
+}
