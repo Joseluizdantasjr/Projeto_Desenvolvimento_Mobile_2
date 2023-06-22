8a27fbe524576eac10c879593ba745e9acc7d7e4
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2023-06-22 20:02:27 -0300

    Add files via upload

---
+++ AppFilmes/src/pages/RegisterPage/index.js
@@ -0,0 +1,22 @@
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import styles from "./styles";
+import { Link } from "@react-navigation/native";
+
+export default function RegisterPage()
+{
+    return (
+        <View style={styles.container}>
+            <Image source={require('../../assets/imgs/logo.png')} style={styles.imgLogo}/>
+            <TextInput placeholder="email" style={styles.input}/>
+            <TextInput placeholder="username" style={styles.input}/>
+            <TextInput placeholder="senha" style={styles.input}/>
+            <TouchableOpacity style={styles.btnLogin}>
+                <Text>Registrar-se</Text>
+            </TouchableOpacity>
+            <View style={styles.viewLinks}>
+                <Link to={{screen: "LoginPage"}}>Login</Link>
+                <Link to={{screen: "ForgetPasswordPage"}}>Esqueceu a senha?</Link>
+            </View>
+        </View>
+    )
+}
